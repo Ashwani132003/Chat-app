1a8d029adfbc060ae3953b84325657bb06eaaa42
STAR SIWACH <76273539+Ashwani132003@users.noreply.github.com>
2022-06-27 12:53:57 +0530

    Update client.py

--- client.py
+++ client.py
@@ -3,7 +3,11 @@ import streamlit as st
 import socket
 from textblob import TextBlob
 
-def host():
+
+
+def client():
+    host = st.text_input(str('Enter the host name: '))
+    
     s=socket.socket()
     st.write('Server will srart at host: ',host)
     port = 8080
@@ -14,16 +18,14 @@ def host():
         incoming_message = s.recv(1024)
         incoming_message = incoming_message.decode()
         blob = TextBlob(incoming_message)
-        incoming_message = blob.translate(to='es')
+        try:
+            incoming_message=blob.translate(from_lang = 'en', to = 'en')
+        except:
+            pass
+        #  = blob.translate(from_lan='en', to='en')
         st.write('Friend >>', incoming_message)
         message = st.text_input(str('You >>'))
         message = message.encode()
-        s.send(message)
-        st.success("Sent")
-
-def client():
-    host = st.text_input(str('Enter the host name: '))
-    if host :
-        st.write('Starting')
-        host()
+        if s.send(message):
+            st.success("Sent")
     
